409b3d1c1d21714687ee434ccb48f1a1dcdc2f72
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2019-08-22 01:18:31 +0530

    Connect Node at same Level

--- Connect_nodes_at_same_level.cpp
+++ Connect_nodes_at_same_level.cpp
@@ -2,33 +2,30 @@
 #include<queue>
 using namespace std;
 
-template<class T>
 struct node{
-  	T data;
-  	node<T>* left;
-  	node<T>* right;
-  	node<T>* next;
+  	char data;
+  	node* left;
+  	node* right;
+  	node* next;
 };
 
-template<class T>
-node<T>* CreateNode(T data){
-	node<T>* temp = new node<T>;
+node* CreateNode(char data){
+	node* temp = new node;
   	temp->data = data;
   	temp->left = temp->right = temp->next = NULL;
   	return temp;
 }
 
-template<class T>
-void ConnectNodes(node<T>* root){
+void ConnectNodes(node* root){
   if(root==NULL){
     return;
   }
   else{
-    queue<struct node<T>*> Q;
+    queue<struct node*> Q;
     Q.push(root);
     Q.push(NULL);
     while(!Q.empty()){
-      struct node<T>* t = Q.front();
+      struct node* t = Q.front();
       Q.pop();
       if(t!=NULL){
         t->next = Q.front();
@@ -47,7 +44,7 @@ void ConnectNodes(node<T>* root){
   }
 }
 int main() {
-  	node<char>* root;
+  	node* root;
   	root = CreateNode('A');
   	root->left = CreateNode('B');
   	root->right = CreateNode('C');
@@ -58,19 +55,25 @@ int main() {
   	root->left->left->right = CreateNode('H');
   	root->right->right->right = CreateNode('I');
   	ConnectNodes(root);
-  	node<char>* level1 = root->left;
+  	node* level0 = root;
+  	while(level0 != NULL){
+  		cout<<level0->data;
+  		level0 = level0->next;
+	}
+	cout<<endl;
+  	node* level1 = root->left;
   	while(level1!=NULL){
     	cout<<level1->data<< " ";
     	level1 = level1->next;
   	}
   	cout<<endl;
-  	node<char>* level2 = root->left->left;
+  	node* level2 = root->left->left;
   	while(level2!=NULL){
     	cout<<level2->data<< " ";
     	level2 = level2->next;
   	}
   	cout<<endl;
-  	node<char>* level3 = root->left->left->right;
+  	node* level3 = root->left->left->right;
   	while(level3!=NULL){
 	    cout<<level3->data<< " ";
 	    level3 = level3->next;
