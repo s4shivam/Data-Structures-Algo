a0bd9a3a08846b03445c5491d08e5cf371079fd8
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2020-08-20 00:02:43 +0530

    Updating pair_in_array_with_sum and optimize the current solution with hashing to avoid sorting the array.

--- pair_in_array_with_sum.cpp
+++ pair_in_array_with_sum.cpp
@@ -1,8 +1,27 @@
 #include<iostream>
 #include <vector>
+#include<map>
 #include<algorithm>
 using namespace std;
-void findpair(vector<int>&V ,int sum)
+void findpair_using_HASHING(vector<int>&V ,int sum)
+{
+  map<int,int> m;
+  int diff;
+  for(int i=0;i<V.size();i++)
+  {
+    diff = sum - V[i];
+    
+    auto itr = m.find(diff);
+    if(itr!=m.end()){  
+      cout<<V[i]<<" "; 
+      cout<<diff ;
+      return ;
+    }
+    m[V[i]] = diff;
+  }
+  return ;
+}
+void findpair_using_TWO_POINTER(vector<int>&V ,int sum)
 {
   sort(V.begin(),V.end());
   int l = 0;
@@ -23,6 +42,7 @@ int main()
 {
   vector<int> V {5,2,4,3,9,7,11};
   int sum = 8;
-  findpair(V,sum);
+  findpair_using_TWO_POINTER(V,sum);
+  findpair_using_HASHING(V,sum);
   return 0;
 }
