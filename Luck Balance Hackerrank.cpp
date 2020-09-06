ea04e3d6260a3b0a302ed741efe99c79e23088ec
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2020-09-07 00:10:03 +0530

    solving HackerRank problem "Luck Balance"
    https://www.hackerrank.com/challenges/luck-balance/problem

---
+++ Luck Balance Hackerrank.cpp
@@ -0,0 +1,26 @@
+bool f(int x,int y){
+    return x>y;
+}
+// Complete the luckBalance function below.
+int luckBalance(int k, vector<vector<int>> contests) {
+    vector<int> V;
+    int luck = 0;
+    for(int i=0;i<contests.size();i++){
+        if(contests[i][1] == 1){
+            V.push_back(contests[i][0]);
+        }
+        if(contests[i][1] == 0){
+            luck+= contests[i][0];
+        }
+    }
+    sort(V.begin(),V.end(),f);
+    for(int i=0;i<V.size();i++){
+        if(i<k){
+            luck+=V[i];
+        }
+        else{
+            luck-=V[i];
+        }
+    }
+    return luck;
+}
