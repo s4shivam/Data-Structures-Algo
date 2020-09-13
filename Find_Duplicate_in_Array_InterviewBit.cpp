5dbe59fec919c04cee060f1226b6f3bb14cd68ff
Shivam Agrawal <43890874+s4shivam@users.noreply.github.com>
2020-09-13 17:57:22 +0530

    Find_Duplicate_in_Array_InterviewBit https://www.interviewbit.com/problems/find-duplicate-in-array/
    https://www.interviewbit.com/problems/find-duplicate-in-array/

---
+++ Find_Duplicate_in_Array_InterviewBit.cpp
@@ -0,0 +1,15 @@
+#include<cmath>
+int Solution::repeatedNumber(const vector<int> &A) {
+    int slow = A[0];
+  int fast = A[A[0]];
+  while(slow!=fast){
+    slow = A[slow];
+    fast = A[A[fast]];
+  }
+  fast = 0;
+  while(slow!=fast){
+    slow = A[slow];
+    fast = A[fast];
+  }
+  return slow;
+}
